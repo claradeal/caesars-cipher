201f752cea59f9cfd2518328023343f36058c3fd
claradeal <clara.deal@gmail.com>
2021-01-21 14:07:23 -0500

    Add files via upload
    initial commit

---
+++ script.js
@@ -0,0 +1,31 @@
+function cipher(codeStr, shift) {
+  if (!codeStr || typeof codeStr !== "string") {
+    codeStr = "";
+  }
+  if (typeof shift !== "number") {
+    shift = 13;
+  }
+  let newStr = "";
+  // for each character in the string that is a letter, shift its unicode value, then convert to new character
+  for (let i = 0; i < codeStr.length; i++) {
+    if (codeStr.charCodeAt(i) < 65 || codeStr.charCodeAt(i) > 91) {
+      newStr = newStr.concat(codeStr.charAt(i));
+    } else if (codeStr.charCodeAt(i) > 64 && codeStr.charCodeAt(i) < 78) {
+      let newIndex = codeStr.charCodeAt(i) + shift;
+      let res = String.fromCharCode(newIndex);
+      newStr = newStr.concat(res);
+    } else if (codeStr.charCodeAt(i) > 77 && codeStr.charCodeAt(i) < 92) {
+      let newIndex = codeStr.charCodeAt(i) - shift;
+      let res = String.fromCharCode(newIndex);
+      newStr = newStr.concat(res);
+    }
+  }
+  return newStr;
+}
+
+// Code below is for running without a UI
+let output = cipher("SERR PBQR PNZC");
+console.log("The answer is " + output);
+console.log(
+  "The next one is " + cipher("GUR DHVPX OEBJA SBK WHZCF BIRE GUR YNML QBT")
+);
